5c6b24a4b2462b7a83c5cdd2c08ee45284c00117
Darshan Raul <dar.raul@gmail.com>
2019-03-26 02:22:44 +0530

    added cards

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
 // Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow,Button,Image,BasicCard} = require('actions-on-google');
+const {dialogflow,Button,Image,BasicCard,Suggestions,SimpleResponse} = require('actions-on-google');
 const axios = require('axios');
 
 // Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
-var name='';
+var name=[];
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
 
@@ -16,47 +16,41 @@ app.intent('profile name', (conv, {profile_name}) => {
   
  
 
-  name = profile_name;
- /*
+  //name = profile_name;
+
  var config = {
   headers: {'profile': 'darshan'}
 };
- axios.get('https:/-1.amazonaws.com/dev/s3/buckets',config)
+ axios.get('https://2f7wrz7c6b.execute-api.ap-south-1.amazonaws.com/dev/s3/buckets',config)
   .then(response => {
     console.log(response.data);
-   name=JSON.stringify(response.data);
+   name=response.data.buckets;
   })
   .catch(error => {
     console.log(error);
-  }); */
+  }); 
     // Respond with the user's lucky number and end the conversation.
   
     conv.ask('Hi ' + name + '.Which A.W.S service do you want to use ?');
 });
 app.intent('service name', (conv, {service_name}) => {
   const service = service_name;
-    conv.ask('ok ' + name + '.the following are the operations you can do in ' + service);
-  conv.ask(new BasicCard({
- 
-  subtitle: 'Use this to create a bucket',
-  title: 'CREATE BUCKET',
+  if(service=="S3"){
+    conv.ask(new SimpleResponse({
+  speech: 'These are the buckets in your S3 account',
+  text: 'These are the buckets in your S3 account',
+}));
+    conv.ask(new BasicCard({
+  text: '${name[0]}', // Note the two spaces before '\n' required for
+                               // a line break to be rendered in the card.
+  
+  title: 'Buckets',
   
   display: 'CROPPED',
 }));
-   conv.ask(new BasicCard({
- 
-  subtitle: 'Use this to delete a bucket',
-  title: 'DELETE BUCKET',
-  
-  display: 'CROPPED',
-}));
-  conv.ask(new BasicCard({
- 
-  subtitle: 'List the buckets in your account',
-  title: 'LIST BUCKETS',
-  
-  display: 'CROPPED',
-}));
+    conv.ask('the following are the operations you can do in ' + service);
+  conv.ask(new Suggestions(['Create Bucket','Delete Bucket','Upload file to a bucket']));
+  }
   conv.ask('What action would you like to make');
 });
 
